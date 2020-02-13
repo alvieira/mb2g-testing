4bc552a87f9a959d47ce2b391354e115eab146bf
Aloisio Vieira <aloisio.vieira@gmail.com>
2020-02-12 23:21:02 -0300

    Using Maven Surefire for Spock Tests

---
+++ src/test/java/guru/springframework/JavaHelloWorldTest.java
@@ -0,0 +1,18 @@
+package guru.springframework;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import guru.springframework.JavaHelloWorld;
+
+public class JavaHelloWorldTest {
+
+	@Test
+	public void testGetHello() {
+		JavaHelloWorld javaHelloWorld = new JavaHelloWorld();
+
+		assertEquals("Hello World", javaHelloWorld.getHello());
+	}
+
+}
